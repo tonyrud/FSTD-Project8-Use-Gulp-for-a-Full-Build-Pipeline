3654837e0c57555c8c55293c907d8089d5a2c9af
tonyrud <tonrudny@gmail.com>
2017-04-01 22:04:20 -0400

    updated eslint options

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,6 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "rules": { 
-        "no-console": 0,
-        "no-debugger": 0, 
-        "no-unused-vars": 0 
+        "no-console": 0
     }
 };
